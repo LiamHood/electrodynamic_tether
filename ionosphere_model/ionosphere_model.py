63e0f221343385084b9b1970ffa91c6c38898bb2
Liam Hood <laser4716@gmail.com>
2023-04-06 21:30:49 -0500

    created python file for neural net

---
+++ ionosphere_model/ionosphere_model.py
@@ -0,0 +1,9 @@
+# import numpy as np
+import pandas as pd
+# import matplotlib.pyplot as plt
+# import keras
+# from keras.datasets import imdb
+# from keras import layers
+
+atmo_data = pd.read_csv("electron density.csv")
+print(atmo_data.head())
