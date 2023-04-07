dc827d8649a9704bcc25b3056fb5b366051a4abb
Liam Hood <laser4716@gmail.com>
2023-04-07 14:31:51 -0500

    updated python model

--- ionosphere_model/ionosphere_model.py
+++ ionosphere_model/ionosphere_model.py
@@ -1,9 +1,24 @@
-# import numpy as np
+import numpy as np
 import pandas as pd
+import tensorflow as tf
 # import matplotlib.pyplot as plt
-# import keras
-# from keras.datasets import imdb
-# from keras import layers
+import keras
+from keras import layers
 
 atmo_data = pd.read_csv("electron density.csv")
 print(atmo_data.head())
+print(atmo_data["Local Time"].head())
+
+inputs = np.vstack((atmo_data["Local Time"], atmo_data["Jdays"], atmo_data["Altitude"], atmo_data["Latitude"],
+                    atmo_data["Longitude"])).astype(np.float32)
+targets = np.vstack((atmo_data["Density"])).astype(np.float32)
+
+model = keras.Sequential([
+    layers.Dense(16, activation="relu"),
+    layers.Dense(16,activation="relu"),
+    layers.Dense(1,activation="sigmoid")
+])
+
+model.compile(optimizer="rmsprop",
+              loss="")
+
